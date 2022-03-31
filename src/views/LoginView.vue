4c5539b21ab51207cd6f0ae24f9651baf00541dc
devpatcher <starlight618628@yahoo.com>
2022-03-31 11:45:17 -0400

    implemented login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="login">
-    <h1>This is login page</h1>
+  <title>Login</title>
+  <nav>
+    <a href="/">
+      <img alt="Logo" src="../assets/logo.png" class="logo">
+    </a>
+    <a href="#" class="float-right">
+      <img alt="Logo" src="../assets/avatar.png" class="avatar">
+    </a>
+  </nav>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col col-lg-4 col-md-6 col-sm-8 col-xs-12 login-form">
+        <div class="login-form-title">
+          Sign In
+        </div>
+        <div class="row">
+          <input class="login-form-input" placeholder="E-mail or Username" type="email">
+          <input class="login-form-input" placeholder="Password" type="password">
+        </div>
+        <div class="row">
+          <button class="btn btn-primary login-form-submit" data-bs-target="#collapseTarget" data-bs-toggle="collapse">
+            Continue
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .login {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
